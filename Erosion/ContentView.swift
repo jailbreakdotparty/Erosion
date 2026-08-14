ebaf896295a1f254666853564cb84e4c1f1c97c3
lunginspector <138542747+lunginspector@users.noreply.github.com>
2026-08-14 16:54:07 -0700

    Initial Commit

---
+++ Erosion/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Erosion
+//
+//  Created by lunginspector on 8/14/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Erosion/ErosionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ErosionApp.swift
+//  Erosion
+//
+//  Created by lunginspector on 8/14/26.
+//
+
+import SwiftUI
+
+@main
+struct ErosionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
